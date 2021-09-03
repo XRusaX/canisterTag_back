cbffa267c10093d2b5a202482493b51fdbd17e10
Семен Предыбайло <s.predybailo@ao-it.ru>
2021-09-03 13:32:08 +0300

    Добавлены типы тестов

--- HmcTestCommon/src/ru/aoit/hmc/test/shared/HmcTestType.java
+++ HmcTestCommon/src/ru/aoit/hmc/test/shared/HmcTestType.java
@@ -3,7 +3,26 @@ package ru.aoit.hmc.test.shared;
 import ru.nppcrts.common.shared.cd.UILabel;
 
 public enum HmcTestType {
-	@UILabel(label="Светодиод")
+	@UILabel(label = "Часы")
+	RTC, //
+	@UILabel(label = "Термодатчик")
+	THERMO, //
+	@UILabel(label = "Моточасы")
+	WORK_TIME, //
+	@UILabel(label = "Компрессор")
+	RELAY,
+	@UILabel(label = "Wi-fi")
+	WIFI,
+	@UILabel(label = "Датчик уровня жидкости")
+	CAP_SENSOR,
+	@UILabel(label = "Светодиод")
 	LED, //
-
+	@UILabel(label = "Звук")
+	BUZZ, //
+	@UILabel(label = "Кнопки")
+	DEMUX, //
+	@UILabel(label = "READER")
+	READER, //
+	@UILabel(label = "Память")
+	FLASH, //
 }
