57f0dac28c7325c50a98c694a2d6a5bcef3d3837
Семен Предыбайло <s.predybailo@ao-it.ru>
2021-10-07 20:17:17 +0300

    Встроил в интерфейс предварительную версию страницы входа

--- HmcRfidServer/src/com/ma/hmcrfidserver/client/HmcRfidServer.java
+++ HmcRfidServer/src/com/ma/hmcrfidserver/client/HmcRfidServer.java
@@ -33,56 +33,23 @@ public class HmcRfidServer extends AppEntryPoint {
 	@Override
 	protected Widget getLoginPage() {
 
-		Label label = new Label("МГЦ");
-		label.getElement().getStyle().setFontWeight(FontWeight.BOLD);
-		label.getElement().getStyle().setFontSize(4,Unit.EM);
-		
-		
-		Button newUser = new Button("Новый пользователь", (ClickHandler) event -> newUser());
-		
-		VertPanel vertPanel = new VertPanel().setSp(10).add(label, newUser, super.getLoginPage());
-		vertPanel.getElement().getStyle().setMargin(4, Unit.EM);
+//		Label label = new Label("МГЦ");
+//		label.getElement().getStyle().setFontWeight(FontWeight.BOLD);
+//		label.getElement().getStyle().setFontSize(4,Unit.EM);
+//		
+//		
+//		Button newUser = new Button("Новый пользователь", (ClickHandler) event -> newUser());
+//		
+//		VertPanel vertPanel = new VertPanel().setSp(10).add(label, newUser, super.getLoginPage());
+//		vertPanel.getElement().getStyle().setMargin(4, Unit.EM);
+//
+//		FlowPanel fp = new FlowPanel();
+//		fp.add(vertPanel);
+//		fp.addStyleName("center");
+//		fp.getElement().getStyle().setBackgroundColor("khaki");
 
-		FlowPanel fp = new FlowPanel();
-		fp.add(vertPanel);
-		fp.addStyleName("center");
-		fp.getElement().getStyle().setBackgroundColor("khaki");
-
-		return fp;
+		return new LoginPanel(hmcService);
 	}
 
-	private void newUser() {
-		TextBox companyName = new TextBox();
-		TextBox adminName = new TextBox();
-		TextBox userEmail = new TextBox();
-		PasswordTextBox adminPassword = new PasswordTextBox();
-		PasswordTextBox adminPassword2 = new PasswordTextBox();
 
-		FlexTableX grid = new FlexTableX();
-		grid.addRow(new Label("Название компании"), companyName);
-		grid.addRow(new Label("Имя администратора"), adminName);
-		grid.addRow(new Label("Пароль администратора"), adminPassword);
-		grid.addRow(new Label("Пароль администратора"), adminPassword2);
-		grid.addRow(new Label("Email"), userEmail);
-
-		OkCancelDialogBox dialogBox = new OkCancelDialogBox("Новый пользователь", grid) {
-			@Override
-			protected boolean onOK() {
-				if (!adminPassword.getText().equals(adminPassword2.getText())) {
-					Window.alert("Ошибка при вводе нового пароля");
-					return false;
-				} else {
-					String hashNew = MD5.calcMD5(adminName.getText() + adminPassword.getText());
-					hmcService.addUserCompany(companyName.getText(), adminName.getText(), hashNew, userEmail.getText(),
-							new AlertAsyncCallback<>("", (v) -> {
-								Window.alert(
-										"Новая компания зарегистрирована, пользователь с правами администратора создан.");
-							}));
-					return true;
-				}
-
-			}
-		};
-		dialogBox.center();
-	}
 }
--- HmcRfidServer/src/com/ma/hmcrfidserver/client/LoginPanel.java
+++ HmcRfidServer/src/com/ma/hmcrfidserver/client/LoginPanel.java
@@ -1,19 +1,25 @@
 package com.ma.hmcrfidserver.client;
 
-
 import com.google.gwt.core.shared.GWT;
+import com.google.gwt.dom.client.Style.FontWeight;
 import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.dom.client.Style.VerticalAlign;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.CheckBox;
+import com.google.gwt.user.client.ui.HasVerticalAlignment;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.PasswordTextBox;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
+import com.ma.common.gwtapp.client.AlertAsyncCallback;
 import com.ma.common.gwtapp.client.auth.Constants;
 import com.ma.common.gwtapp.client.auth.LoginPageBase;
 import com.ma.common.gwtapp.client.ui.FlexTableX;
+import com.ma.common.gwtapp.client.ui.dialog.OkCancelDialogBox;
+import com.ma.common.shared.MD5;
 
 public class LoginPanel extends LoginPageBase {
 	private final Constants constants = GWT.create(Constants.class);
@@ -21,6 +27,8 @@ public class LoginPanel extends LoginPageBase {
 	private final Label err = new Label();
 	private TextBox user = new TextBox();
 	private PasswordTextBox password = new PasswordTextBox();
+
+	private HmcServiceAsync hmcService;
 
 	@Override
 	protected void onErr(String text) {
@@ -30,7 +38,14 @@ public class LoginPanel extends LoginPageBase {
 		password.addStyleName("myfirst");
 	}
 
-	public LoginPanel() {
+	public LoginPanel(HmcServiceAsync hmcService) {
+		this.hmcService = hmcService;
+		HorizontalPanel labelPanel = new HorizontalPanel();
+		Label label = new Label("МГЦ");
+		label.getElement().getStyle().setFontWeight(FontWeight.BOLD);
+		label.getElement().getStyle().setFontSize(4, Unit.EM);
+		label.getElement().getStyle().setMarginBottom(50, Unit.PX);
+		labelPanel.add(label);
 
 		CheckBox saveData = new CheckBox(constants.savePassword());
 		saveData.getElement().setClassName("input");
@@ -48,26 +63,36 @@ public class LoginPanel extends LoginPageBase {
 		Label userNameLbl = new Label(constants.userName());
 		userNameLbl.setStyleName("status-label");
 		userNameLbl.addStyleName("simple");
+		grid.addRow(labelPanel);
 		grid.addRow(userNameLbl, user);
 		Label passwordLbl = new Label(constants.password());
 		passwordLbl.setStyleName("status-label");
 		passwordLbl.addStyleName("simple");
 		grid.addRow(passwordLbl, password);
-
+//		grid.setWidth("100%");
 		Button loginButton = new Button(constants.login(),
 				(ClickHandler) event -> login2(user.getText(), password.getText(), saveData.getValue()));
-
-		HorizontalPanel hp = new HorizontalPanel();
 		loginButton.setStyleName("button-green");
+		
+		Button newUserButton = new Button("Новый пользователь", (ClickHandler) event -> newUser());
+		newUserButton.setStyleName("button-google");
+		newUserButton.getElement().getStyle().setBackgroundColor("khaki");
+		
+		HorizontalPanel hp = new HorizontalPanel();
+		
 		hp.getElement().getStyle().setMarginTop(50, Unit.PX);
+		hp.setVerticalAlignment(HasVerticalAlignment.ALIGN_MIDDLE);
+		hp.setWidth("100%");
+		hp.setSpacing(10);
 		hp.add(loginButton);
 		hp.add(saveData);
+		hp.add(newUserButton);
 
 		VerticalPanel verticalPanel = new VerticalPanel();
 		verticalPanel.add(err);
 		verticalPanel.add(grid);
 		verticalPanel.add(hp);
-		
+
 //		Button restorePasswordBtn = new Button(constants.forgotPassword(), (ClickHandler) event -> restorePassword());
 //		restorePasswordBtn.getElement().getStyle().setMarginTop(20, Unit.PX);
 //		verticalPanel.add(restorePasswordBtn);
@@ -77,4 +102,40 @@ public class LoginPanel extends LoginPageBase {
 
 		initWidget(verticalPanel);
 	}
+
+	private void newUser() {
+		TextBox companyName = new TextBox();
+		TextBox adminName = new TextBox();
+		TextBox userEmail = new TextBox();
+		PasswordTextBox adminPassword = new PasswordTextBox();
+		PasswordTextBox adminPassword2 = new PasswordTextBox();
+
+		FlexTableX grid = new FlexTableX();
+		grid.addRow(new Label("Название компании"), companyName);
+		grid.addRow(new Label("Имя администратора"), adminName);
+		grid.addRow(new Label("Пароль администратора"), adminPassword);
+		grid.addRow(new Label("Пароль администратора"), adminPassword2);
+		grid.addRow(new Label("Email"), userEmail);
+
+		OkCancelDialogBox dialogBox = new OkCancelDialogBox("Новый пользователь", grid) {
+			@Override
+			protected boolean onOK() {
+				if (!adminPassword.getText().equals(adminPassword2.getText())) {
+					Window.alert("Ошибка при вводе нового пароля");
+					return false;
+				} else {
+					String hashNew = MD5.calcMD5(adminName.getText() + adminPassword.getText());
+					hmcService.addUserCompany(companyName.getText(), adminName.getText(), hashNew, userEmail.getText(),
+							new AlertAsyncCallback<>("", (v) -> {
+								Window.alert(
+										"Новая компания зарегистрирована, пользователь с правами администратора создан.");
+							}));
+					return true;
+				}
+
+			}
+		};
+		dialogBox.center();
+	}
+
 }
