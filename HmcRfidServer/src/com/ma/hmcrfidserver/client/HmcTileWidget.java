7c4c5d88db841623f56aadc026facacf3819b6e0
Семен Предыбайло <s.predybailo@ao-it.ru>
2021-10-19 16:23:55 +0300

    Начал переделку UI

--- HmcRfidServer/src/com/ma/hmcrfidserver/client/HmcTileWidget.java
+++ HmcRfidServer/src/com/ma/hmcrfidserver/client/HmcTileWidget.java
@@ -3,8 +3,6 @@ package com.ma.hmcrfidserver.client;
 import com.google.gwt.event.dom.client.LoadEvent;
 import com.google.gwt.event.dom.client.LoadHandler;
 import com.google.gwt.resources.client.ImageResource;
-import com.google.gwt.user.client.DOM;
-import com.google.gwt.dom.client.Element;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.HasVerticalAlignment;
 import com.google.gwt.user.client.ui.HorizontalPanel;
@@ -19,26 +17,35 @@ import com.ma.hmc.iface.report.shared.HmcReportStatus;
 public class HmcTileWidget extends Composite implements ImageResources {
 	public HmcTileWidget(CDObject t) {
 		VerticalPanel panel = new VerticalPanel();
-		HorizontalPanel hPanel = new HorizontalPanel();
+		HorizontalPanel topHorPanel = new HorizontalPanel();
+		Label tileName = new Label();
+		tileName.setStyleName("my-title");
+		topHorPanel.add(tileName);
+		tileName.setTitle("с/н: " + t.get("serialNumber"));
+		HorizontalPanel middleHorPanel = new HorizontalPanel();
 		VertPanel sp = new VertPanel();
-		Label ph = new Label();
-		ph.setSize("120px", "1px");
-		sp.add(ph);
+		Label placeHolder = new Label();
+		placeHolder.setSize("120px", "1px");
+		sp.add(placeHolder);
 		panel.setStyleName("rounded-panel");
 		String hmcType = t.get("hmcType");
 		Image hmcImg;
 		switch (hmcType) {
 		case "HMC_1":
 			hmcImg = new Image(IMAGE_RESOURCES.hmc1());
+			tileName.setText("МГЦ 1");
 			break;
 		case "HMC_2":
 			hmcImg = new Image(IMAGE_RESOURCES.hmc2());
+			tileName.setText("МГЦ 2");
 			break;
 		case "HMC_3":
 			hmcImg = new Image(IMAGE_RESOURCES.hmc3());
+			tileName.setText("МГЦ 3");
 			break;
 		case "HMC_4":
 			hmcImg = new Image(IMAGE_RESOURCES.notFound());
+			tileName.setText("МГЦ 4");
 			break;
 		default:
 			hmcImg = new Image(IMAGE_RESOURCES.noType());
@@ -50,23 +57,24 @@ public class HmcTileWidget extends Composite implements ImageResources {
 			@Override
 			public void onLoad(LoadEvent event) {
 				hmcImg.setPixelSize(120, hmcImg.getHeight() * 120 / hmcImg.getWidth());
-				sp.remove(ph);
+				sp.remove(placeHolder);
 				hmcImg.setVisible(true);
 			}
 		});
 
 		ProgressBar disLevel = new ProgressBar();
 		disLevel.setSpacing(10);
-		hPanel.setVerticalAlignment(HasVerticalAlignment.ALIGN_MIDDLE);
+		middleHorPanel.setVerticalAlignment(HasVerticalAlignment.ALIGN_MIDDLE);
 
 		sp.add(hmcImg);
 		hmcImg.setVisible(false);
 
-		hPanel.add(sp);
+		middleHorPanel.add(sp);
+		middleHorPanel.add(disLevel);
 
-		hPanel.add(disLevel);
+		panel.add(topHorPanel);
+		panel.add(middleHorPanel);
 
-		panel.add(hPanel);
 		InlineHTML label = new InlineHTML();
 		label.setStyleName("status-label", true);
 
