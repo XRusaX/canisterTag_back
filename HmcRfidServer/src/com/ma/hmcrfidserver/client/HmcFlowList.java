297bb8975c44669fd86102d4d54632810569bf95
Семен Предыбайло <s.predybailo@ao-it.ru>
2021-10-19 16:24:14 +0300

    Добавил отображение отчетов для МГЦ

--- HmcRfidServer/src/com/ma/hmcrfidserver/client/HmcFlowList.java
+++ HmcRfidServer/src/com/ma/hmcrfidserver/client/HmcFlowList.java
@@ -2,16 +2,31 @@ package com.ma.hmcrfidserver.client;
 
 import com.google.gwt.user.client.ui.Widget;
 import com.ma.common.gwtapp.client.commondata.CommonDataFlowList;
+import com.ma.common.gwtapp.client.ui.ContextMenu;
 import com.ma.commonui.shared.cd.CDObject;
 
-public class HmcFlowList extends CommonDataFlowList {
+public abstract class HmcFlowList extends CommonDataFlowList {
 
 	public HmcFlowList() {
+		super("selection");
 	}
 
 	@Override
 	protected Widget createWidget(CDObject t) {
 		return new HmcTileWidget(t);
 	}
+
+	@Override
+	protected void prepareContextMenu(ContextMenu menu, CDObject t, Runnable onPrepared) {
+		super.prepareContextMenu(menu, t, onPrepared);
+		if (t != null) {
+			menu.addItem("Показать отчеты", () -> {
+				showReports();
+			});
+		}
+
+	}
+
+	abstract void showReports();
 
 }
