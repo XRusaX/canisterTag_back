e5cf93d8ae7c72eaacc66c06f31af5ff6188b613
Семен Предыбайло <s.predybailo@ao-it.ru>
2021-10-22 19:40:15 +0300

    Начал работу над списком сотрудников

---
+++ HmcRfidServer/src/com/ma/hmcrfidserver/client/OperatorList.java
@@ -0,0 +1,14 @@
+package com.ma.hmcrfidserver.client;
+
+import com.google.gwt.core.client.GWT;
+import com.ma.common.gwtapp.client.commondata.CommonListPanel;
+
+public class OperatorList extends CommonListPanel {
+
+	static MyDataGridResources resource = GWT.create(MyDataGridResources.class);
+
+	public OperatorList(String title, Integer refreshPeriod, String styleName) {
+		super(title, refreshPeriod, styleName, resource);
+	}
+
+}
