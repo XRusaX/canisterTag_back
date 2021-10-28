c023ada227d44fce17f0fb61ccbd9c857208f9a0
Семен Предыбайло <s.predybailo@ao-it.ru>
2021-10-28 20:08:34 +0300

    Ресурсы awesomefonts

---
+++ HmcRfidServer/src/com/ma/hmcrfidserver/client/FontAwesomeBundle.java
@@ -0,0 +1,30 @@
+package com.ma.hmcrfidserver.client;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.CssResource;
+import com.google.gwt.resources.client.DataResource;
+
+public interface FontAwesomeBundle extends ClientBundle {
+	public static final FontAwesomeBundle INSTANCE = GWT.create(FontAwesomeBundle.class);
+
+	@Source("/resources/css/fontawesome-all.min.css")
+	@CssResource.NotStrict
+	CssResource fontAwesome();
+
+	@DataResource.DoNotEmbed
+	@DataResource.MimeType("application/font-woff")
+	@Source("/resources/fonts/fa-brands-400.woff")
+	DataResource faBrands400woff();
+
+	@DataResource.DoNotEmbed
+	@DataResource.MimeType("application/font-woff")
+	@Source("/resources/fonts/fa-regular-400.woff")
+	DataResource faRegular400woff();
+
+	@DataResource.DoNotEmbed
+	@DataResource.MimeType("application/font-woff")
+	@Source("/resources/fonts/fa-solid-900.woff")
+	DataResource faSolid900woff();
+
+}
