b8f3ada5f9304c0e5ad3adf4ea9831a26094d0d2
Vladimir Krupskii <v.krupskii@ao-it.ru>
2021-11-11 19:01:54 +0300

    s

--- HmcInterface/src/com/ma/hmc/iface/opsync/OpSync.java
+++ HmcInterface/src/com/ma/hmc/iface/opsync/OpSync.java
@@ -5,4 +5,21 @@ import java.util.List;
 public class OpSync {
 	public long lastSync;
 	public List<Operator> operators;
+	
+	public static class Operator {
+		public long id; // id или 0 для вновь созданного оператора
+		public String name;
+		public boolean removed;
+		public long modifTime; // UTC3 время изменения
+
+		public Operator() {
+		}
+
+		public Operator(long id, String name, boolean removed, long modifTime) {
+			this.id = id;
+			this.name = name;
+			this.removed = removed;
+			this.modifTime = modifTime;
+		}
+	}
 }
