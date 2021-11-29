2a1db25c257c3aa5132d77758d265ece2d4100e3
Семен Предыбайло <s.predybailo@ao-it.ru>
2021-11-29 19:47:24 +0300

    Добавлена генерация workingDir по названию приложения

--- HmcAppCommon/src/com/ma/hmcapp/HmcWorkingDir.java
+++ HmcAppCommon/src/com/ma/hmcapp/HmcWorkingDir.java
@@ -1,15 +1,16 @@
 package com.ma.hmcapp;
 
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
 import com.ma.appcommon.WorkingDir;
 import com.ma.common.app.AppData;
 
 @Component
-public class HmcWorkingDir extends WorkingDir{
+public class HmcWorkingDir extends WorkingDir {
 
-	public HmcWorkingDir() {
-		super(AppData.getWorkingDir("HMC"));
+	public HmcWorkingDir(ApplicationContext context) {
+		super(AppData.getWorkingDir(context.getApplicationName().isEmpty() ? "dev" : context.getApplicationName()));
 	}
 
 }
