71e8d7131c767f41b71b28695ff1c9f962b37213
Семен Предыбайло <s.predybailo@ao-it.ru>
2021-12-17 14:07:14 +0300

    Структура данных RFID обновлена до v1.3

--- HmcInterface/src/com/ma/hmc/iface/rfid/ruslandata/RfidDataUtils.java
+++ HmcInterface/src/com/ma/hmc/iface/rfid/ruslandata/RfidDataUtils.java
@@ -4,12 +4,12 @@ import java.io.UnsupportedEncodingException;
 
 public class RfidDataUtils {
 	public static byte[] getKey(RfidData data) throws UnsupportedEncodingException {
-		return getKey(data.UNIQUE_ID, data.CANISTER_VOLUME_ML, data.CANISTER_MANUFACTURER_NAME, data.CANISTER_NAME,
-				data.CANISTER_CONSUMPTION_ML_M3, data.CANISTER_CONSUMPTION2_ML_M3, data.CANISTER_AERATION_MIN);
+		return getKey(data.CAN_UNIQUE_ID, data.CAN_VOLUME_ML, data.CAN_MANUFACTURER_NAME, data.CAN_DESINFICTANT_NAME,
+				data.CAN_ACTIVE_INGRIDIENT_NAME, data.CAN_INGRIDIENT_CONCENTRATION);
 	}
 
-	public static byte[] getKey(Integer id, Integer volume, String manufacturer, String name, Integer consumption,
-			Integer consumption2, Integer aeration) throws UnsupportedEncodingException {
-		return ("" + id + volume + manufacturer + name + consumption + consumption2 + aeration).getBytes("utf-8");
+	public static byte[] getKey(Integer id, Integer volume, String manufacturer, String name,
+			String activeIngridientName, Integer concentration) throws UnsupportedEncodingException {
+		return ("" + id + volume + manufacturer + name + activeIngridientName + concentration).getBytes("utf-8");
 	}
 }
