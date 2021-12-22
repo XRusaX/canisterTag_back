9c3c9d50f47615b55b15c0764554c86d4b22fa2a
Семен Предыбайло <s.predybailo@ao-it.ru>
2021-12-22 19:51:05 +0300

    Убрал лищние поля из ср-ва

--- HmcAppCommon/src/com/ma/hmcapp/entity/Agent.java
+++ HmcAppCommon/src/com/ma/hmcapp/entity/Agent.java
@@ -20,22 +20,24 @@ public class Agent {
 	@Column(unique = true)
 	@UILabel(label = "Название", sortable = true)
 	private String name;
-
-	@UILabel(label = "Расход, мл/м3")
-	private int consumption_ml_m3;
-	@UILabel(label = "Расход профилакт., мл/м3")
-	private int consumption2_ml_m3;
-	@UILabel(label = "Время аэрации, мин.")
-	private int aeration_min;
-
-	@UILabel(label = "Срок годности, мес.")
-	private Integer shelfLife_months;
 	
 	@UILabel(label = "Действующее в-во")
 	private String ingridientName;
 	
 	@UILabel(label = "Концентрация, %")
 	private int concentration;
+
+//	@UILabel(label = "Расход, мл/м3")
+//	private int consumption_ml_m3;
+//	@UILabel(label = "Расход профилакт., мл/м3")
+//	private int consumption2_ml_m3;
+//	@UILabel(label = "Время аэрации, мин.")
+//	private int aeration_min;
+
+	@UILabel(label = "Срок годности, мес.")
+	private Integer shelfLife_months;
+	
+
 	
 	public Agent() {
 	}
@@ -43,9 +45,9 @@ public class Agent {
 	public Agent(String name, int consumption_ml_m3, int consumption2_ml_m3, int aeration_min,
 			Integer shelfLife_months, String ingridientName, int concentration) {
 		this.name = name;
-		this.consumption_ml_m3 = consumption_ml_m3;
-		this.consumption2_ml_m3 = consumption2_ml_m3;
-		this.aeration_min = aeration_min;
+//		this.consumption_ml_m3 = consumption_ml_m3;
+//		this.consumption2_ml_m3 = consumption2_ml_m3;
+//		this.aeration_min = aeration_min;
 		this.shelfLife_months = shelfLife_months;
 		this.ingridientName = ingridientName;
 		this.concentration = concentration;
