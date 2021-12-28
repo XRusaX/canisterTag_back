7ccb6756cda5d06ac7cc1ee51fb08a56ef4a8e72
Семен Предыбайло <s.predybailo@ao-it.ru>
2021-12-28 18:10:53 +0300

    Квота для передачи на клиент записи меток

--- HmcInterface/src/com/ma/hmc/iface/rfid/rfiddata/QuotaDTO.java
+++ HmcInterface/src/com/ma/hmc/iface/rfid/rfiddata/QuotaDTO.java
@@ -3,12 +3,12 @@ package com.ma.hmc.iface.rfid.rfiddata;
 import java.io.Serializable;
 
 @SuppressWarnings("serial")
-public class Quota implements Serializable {
+public class QuotaDTO implements Serializable {
 	public String name;
 	public double concentration;
 	public int volume;
 
-	public Quota(String name, double concentration, int volume) {
+	public QuotaDTO(String name, double concentration, int volume) {
 		this.name = name;
 		this.concentration = concentration;
 		this.volume = volume;
@@ -16,7 +16,7 @@ public class Quota implements Serializable {
 
 	@Override
 	public String toString() {
-		return name + " " + concentration + "%" + " (" + volume + ")";
+		return name + " " + concentration + "%" + " (" + volume + "мл.)";
 	}
 
 }
